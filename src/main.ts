3b0418e2a018dff53ececc2a7338fc3b7039351d
Ben Lee <Ben.m.lee@outlook.com>
2023-09-22 15:32:58 +1000

    [add] messaging

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,26 @@ let devices: Device[] = [
   new Device({ name: 'Router', ip: '10.1.1.1', status: DeviceStatus.Up }),
 ]
 
+let maps = new Map<string, NetworkMap>()
+
 const wss = new WebSocketServer({ port: 3001 })
 
 wss.on('connection', (ws, message) => {
   console.log("New Connection");
-  ws.send('Hey there')
-  ws.on('message', (message) => {
-    console.log(message);
+  ws.on('message', (raw) => {
+    let message = JSON.parse(raw.toString()) as Message
+    console.log(message.data);
+
+    if (message.type === MsgType.SaveMap) {
+      let map = message.data as NetworkMap
+      maps.set(map.map_name, map)
+    }
+    else if (message.type === MsgType.GetMap) {
+      let m = maps.get(message.data)
+      if (m) ws.send(JSON.stringify(msg(MsgType.Reply, m, message.reply)))
+      else ws.send(JSON.stringify(msg(MsgType.Reply, null, message.reply)))
+    }
+
   })
   ws.on('error', (message) => {
     console.log("Error");
@@ -35,3 +48,34 @@ const server = createServer((req, res) => {
 }).listen(3000)
 
 console.log("Webserver Started - http://localhost:3000");
+
+function msg(type: MsgType, data: any, reply: number | null = null) {
+  return { type, data, reply }
+}
+
+enum MsgType {
+  Reply,
+  SaveMap,
+  GetMap
+}
+
+interface Message {
+  type: MsgType,
+  data: any,
+  reply: number | null
+}
+
+interface NetworkMap {
+  map_name: string;
+  rooms: MapRoom[][];
+  device: MapDevice[]
+}
+
+interface MapRoom {
+  global_rect: number[]
+}
+
+interface MapDevice {
+  device_name: string;
+  position: number[];
+}
